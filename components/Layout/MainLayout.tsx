b34d8a5705a91cecb4051b64c98115861361a1ad
Joycrown <olaniyanayoade999@gmail.com>
2026-02-12 00:40:40 +0100

    Adding sentry and posthog for analytics

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -14,6 +14,7 @@ export default function MainLayout({
   const pathname = usePathname()
   const isPublicRoute =
     publicRoutes.includes(pathname || '') ||
+    (pathname?.startsWith('/auth/') ?? false) ||
     (pathname?.startsWith('/profile/') ?? false) ||
     (pathname?.startsWith('/message/') ?? false)
 
