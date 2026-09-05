44bbe8985000d33bf4759a47d846037ebf6fb07f
Joycrown <olaniyanayoade999@gmail.com>
2026-09-05 14:46:37 +0100

    Update check-admins.js
    changes and updates

--- check-admins.js
+++ check-admins.js
@@ -1,6 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
+
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
